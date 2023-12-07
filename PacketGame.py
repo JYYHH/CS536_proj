00498988c24acb5289051d997ae77c582d4c23a6
JYYHH <jin511@purdue.edu>
2023-12-07 02:17:12 +0000

    delete parameters file

--- PacketGame.py
+++ PacketGame.py
@@ -1,10 +1,29 @@
 from Manager import *
 
+file_paths = [
+    # 0
+    (
+        "/opt/cs536/Blend_Dataset/parse/Concat/FireVid36_NoFireVid7_parse.txt", 
+        "/opt/cs536/Blend_Dataset/Concat/FireVid36_NoFireVid7.txt"
+    ),
+
+    # 1
+    (
+        "/opt/cs536/Blend_Dataset/parse/One-by-One/FireVid36_NoFireVid7_parse.txt", 
+        "/opt/cs536/Blend_Dataset/One-by-One/FireVid36_NoFireVid7.txt"
+    ),
+    
+    # 2
+    (
+        "/opt/cs536/Blend_Dataset/parse/One-by-One/FireVid36_NoFireVid7_parse.txt", 
+        "/opt/cs536/Blend_Dataset/One-by-One/FireVid36_NoFireVid7.txt"
+    ),
+]
+
 obj = Globel_Manager()
 obj.train_model(
-    "/opt/cs536/Blend_Dataset/parse/Concat/FireVid36_NoFireVid7_parse.txt", 
-    "/opt/cs536/Blend_Dataset/Concat/FireVid36_NoFireVid7.txt",
-    10,
+    *file_paths[1],
+    20,
     32,
     True
 )
